7891bf7d407215ce9bffb0489079c05a35313ca1
Piriyakorn <piriyakorn.sae@gmail.com>
2025-07-10 14:41:07 +0700

    sql

--- obj/Debug/net8.0/MyApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42be46beb128c5c98a40f39b4ec4c53b20856351")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0b2458586ad8b0cdc3c06c471f6a9a4ec30171b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
